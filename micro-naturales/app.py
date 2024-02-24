9fbbe75b8d6aff72bcbf2d01c257dafdfaba83bb
yshungria-uniandes <ys.hungria@uniandes.edu.co>
2024-02-24 18:20:43 -0500

    fix: "se eliminan archivos no necesario"

--- micro-naturales/app.py
+++ micro-naturales/app.py
@@ -1,22 +1,49 @@
+import redis
 from flask import Flask, request, jsonify
-from .models import db, Event
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db/events.db'
-db.init_app(app)
 
-@app.route('/events', methods=['GET'])
-def get_events():
-    events = Event.query.all()
-    return jsonify([{'name': event.name, 'location': event.location, 'severity': event.severity} for event in events])
+# Conexión a Redis: Se establece una conexión con el servidor Redis. En este caso, se asume que Redis está ejecutándose en el mismo contenedor con el nombre de host redis en el puerto predeterminado 6379.
 
-@app.route('/events', methods=['POST'])
-def create_event():
+cache = redis.Redis(host='redis', port=6379)
+
+@app.route('/eventos-naturales', methods=['POST'])
+def eventos_naturales():
     data = request.get_json()
-    event = Event(name=data['name'], location=data['location'], severity=data['severity'])
-    db.session.add(event)
-    db.session.commit()
-    return jsonify({'message': 'Event created successfully'}), 201
+
+    #procesar el evento
+    event_data = {
+        'event_type': data['event_type'],
+        'location': data['location'],
+        'severity': data['severity'],
+    }
+
+    # Almacenar el evento en Redis para persistencia temporal
+    cache.rpush('eventos_naturales', event_data)
+
+    #return 'Evento publicado', 200
+    return 'OK', 200
+    
+    # Enviar el evento a RabbitMQ para procesamiento asíncrono
+    channel.basic_publish(exchange='events', routing_key='natural_events_queue', body=json.dumps(event_data))
+
+    return jsonify({'message': 'Natural event received successfully'}), 200
+
+@app.route('/eventos-naturales', methods=['GET'])
+def get_eventos_naturales():
+    events = []
+    for event in cache.lrange('eventos_naturales', 0, -1):
+        events.append(event.decode('utf-8'))
+
+    return jsonify(events), 200
+
+    # Enviar el evento a RabbitMQ para procesamiento asíncrono
+    channel.basic_publish(exchange='events', routing_key='natural_events_queue', body=json.dumps(events))
+
+@app.route('/' , methods=['GET'])
+def index():
+    return 'Servicio de eventos naturales'
 
 if __name__ == '__main__':
     app.run(debug=True)
