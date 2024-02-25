267e7fd281fb373b34b0b6e3db0cd3456de960f7
Javier Dev <ja.acevedob1@uniandes.edu.co>
2024-02-25 18:10:05 -0500

    Feature : micro seguridad

--- micro-seguridad/app.py
+++ micro-seguridad/app.py
@@ -4,9 +4,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Conexión a Redis: Se establece una conexión con el servidor Redis. En este caso, se asume que Redis está ejecutándose en el mismo contenedor con el nombre de host redis en el puerto predeterminado 6379.
-
-cache = redis.Redis(host='redis', port=6379)
+memoria = redis.Redis(host='redis', port=6379)
 
 @app.route('/eventos-seguridad', methods=['POST'])
 def eventos_seguridad():
@@ -20,25 +18,18 @@ def eventos_seguridad():
     }
 
     # Almacenar el evento en Redis para persistencia temporal
-    cache.rpush('eventos_seguridad', event_data)
-
-    #return 'Evento publicado', 200
-    return 'OK', 200
-    
-    # Enviar el evento a RabbitMQ para procesamiento asíncrono
-    channel.basic_publish(exchange='events', routing_key='natural_events_queue', body=json.dumps(event_data))
+    memoria.rpush('eventos_seguridad', event_data)
 
     return jsonify({'message': 'Security event received successfully'}), 200
 
 @app.route('/eventos-seguridad', methods=['GET'])
 def get_eventos_naturales():
     events = []
-    for event in cache.lrange('eventos_seguridad', 0, -1):
+    for event in memoria.lrange('eventos_seguridad', 0, -1):
         events.append(event.decode('utf-8'))
 
     return jsonify(events), 200
 
-    # Enviar el evento a RabbitMQ para procesamiento asíncrono
     channel.basic_publish(exchange='events', routing_key='natural_events_queue', body=json.dumps(events))
 
 @app.route('/' , methods=['GET'])
@@ -46,4 +37,4 @@ def index():
     return 'Servicio de eventos de seguridad'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
